f30547220a5ac535ab7104a67fc072cf7ba5caa3
Lucas Dorrough <lbdorrou@uark.edu>
2014-11-09 03:11:04 +0000

    UI update 1.2

--- Server/config/routes.rb
+++ Server/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'pages#index'
   resources :sections
   resources :courses
+  resources :pages
 
 
   # The priority is based upon order of creation: first created -> highest priority.
