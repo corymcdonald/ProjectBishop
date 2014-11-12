b74b5de28579dd9aa3c8545fe234fd7852f9f757
maluther <farewellalbatross@gmail.com>
2014-11-12 02:56:53 +0000

    Added the checks for token days, stubbed the findTime function.

--- Schedule/Schedule.rb
+++ Schedule/Schedule.rb
@@ -1,10 +1,10 @@
 Class Schedule
 def arrOfSections
-  @arrOfSections
+    @arrOfSections
 end
 
 def arrOfSections=(value)
-  @arrOfSections = value
+    @arrOfSections = value
 end
 def initialize(arrOfSections)  
     # Instance variables  
@@ -13,29 +13,62 @@ end
 def addSection(courseSection)
     @arrOfSections.push(courseSection)
 end
+def findTime(timeArray, startingLocation, start, finish)
+    #implement this soon
+end
 def genereateSchedules(arrOfCourses)
     if arrOfCourses.length == 0
         return
     else 
-        currentSchedules = null
-        nextSchedules = null
-        temporarySections = null
+        currentSchedules = Array.new
+        nextSchedules = Array.new
+        temporarySections = Array.new
         for i in 0..5
             currentSchedules = nextSchedules
-            temporarySections = null
-            nextSchedules = null
+            temporarySections = Array.new
+            nextSchedules = Array.new
         
             if i == 0
                 temporarySections = courses.where(title: arrOfCourses.get(i).title).order('created_at DESC')
                 temporarySections.each do |section|
-                    nextSchedules.push(section)
+                    nextSchedules.push(Schedule.new(section))
                 end
-            elsif i <5
+            elsif i < 5
                 temporarySections = courses.where(title: arrOfCourses.get(i).title).order('created_at DESC')
                 temporarySections.each do |section|
-                    #canAdd = true
+                    canAdd = true
                     currentSchedules.each do |schedule|
                         schedule.arrOfSections.each do |sectionToCheck|
+                            sectionTimes = section.classTime.split(/ |, |,/)
+                            for i in sectionTimes.length
+                                if sectionTimes[i].include? "Mon"
+                                    startTime = null
+                                    endTime = null
+                                    findTime(sectionTimes, i, s)
+                                        
+                                        
+                                end
+                                if sectionTimes[i].include? "Tue"
+                                    startTime = null
+                                    endTime = null
+                                    findTime(sectionTimes, i, s)
+                                end
+                                if sectionTimes[i].include? "Wed"
+                                    startTime = null
+                                    endTime = null
+                                    findTime(sectionTimes, i, s)
+                                end
+                                if sectionTimes[i].include? "Thu"
+                                    startTime = null
+                                    endTime = null
+                                    findTime(sectionTimes, i, s)
+                                end
+                                if sectionTimes[i].include? "Fri"
+                                    startTime = null
+                                    endTime = null
+                                    findTime(sectionTimes, i, s)
+                                end
+                            end
                             #Add logic to add schedules here, unsure how exactly the start and end times will be stored
                             #Probably something like go through all the days the sectionToCheck has, and see if the section is on that day. If so, see if the times overlap. If they do set the bool to false
                         end
@@ -43,7 +76,7 @@ def genereateSchedules(arrOfCourses)
                     end
                 end
             else
-                
+                #Handle breaks
             end
         end
         return nextSchedules
