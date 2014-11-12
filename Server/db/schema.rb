667a19c450a0b1ed54cbb13395632f5cfa099ab7
wsturner <wsturner.1123@gmail.com>
2014-11-12 06:49:07 +0000

    added email format validation as well as uniqueness verification

--- Server/db/schema.rb
+++ Server/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141111204612) do
+ActiveRecord::Schema.define(version: 20141112063716) do
 
   create_table "courses", force: true do |t|
     t.string   "title"
@@ -68,5 +68,7 @@ ActiveRecord::Schema.define(version: 20141111204612) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
