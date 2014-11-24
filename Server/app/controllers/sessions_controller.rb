90d963ee9dee62faa44d314222fb8be611261677
wsturner <wsturner.1123@gmail.com>
2014-11-24 04:07:01 +0000

    invalid login throws error

--- Server/app/controllers/sessions_controller.rb
+++ Server/app/controllers/sessions_controller.rb
@@ -3,7 +3,13 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # Log the user in and redirect to the user's show page.
+    else
+      flash.now[:danger] = 'Invalid email/password combination' # Not quite right!
     render 'new'
+    end
   end
 
   def destroy
