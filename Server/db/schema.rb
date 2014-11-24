693b0b7029f83d0fa67c2119617782fee38ccd0d
wsturner <wsturner.1123@gmail.com>
2014-11-24 22:41:46 +0000

    full login works along with cookies and logout. needs filter as well as email verification and account editing

--- Server/db/schema.rb
+++ Server/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141122224304) do
+ActiveRecord::Schema.define(version: 20141124215619) do
 
   create_table "classes_lists", force: true do |t|
     t.string   "user"
@@ -74,6 +74,7 @@ ActiveRecord::Schema.define(version: 20141122224304) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
+    t.string   "remember_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
