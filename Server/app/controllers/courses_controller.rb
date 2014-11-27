0391dca36f0d68786baf68ef006bd51cd41c16c5
Cory McDonald <cory@corywmcdonald.com>
2014-11-27 02:07:02 -0600

    Added a show /index for courses

--- Server/app/controllers/courses_controller.rb
+++ Server/app/controllers/courses_controller.rb
@@ -1,10 +1,30 @@
 class CoursesController < ApplicationController
   def index
-    if params[:search]
-      @course = Course.search(params[:search])
-    else
-      @course = Course.all
-
+    @course = Course.all
+    
+    @courses = {}
+    @allcourses = []
+    @temp = []
+    
+    for current in @course
+      @temp.push(current.name[0,4])
+      # @allcourses << [current.name[0,4], current]
     end
+    @uniqcourses = @temp.uniq
+  
+    
+    Rails.logger.info(DateTime.now)
+    # @courses = @allcourses
+    for current in @uniqcourses
+      @courses[current] = @course.select{|x| x.name[0,4] == current }
+    end
+    Rails.logger.info(DateTime.now)
+  end
+  
+  def show
+    @course = Course.find(params[:id])
+    @sections = Section.where('name = ?', @course.name.gsub(' ', ''))
+    @numOfPreReqs =JSON.parse(@course.prereqData).count
+    
   end
 end
