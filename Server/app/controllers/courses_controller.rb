5876c9b7f130b8de53f303436b0d9c7a7c5171c9
Cory McDonald <cory@corywmcdonald.com>
2014-11-27 23:26:40 -0600

    Adding search functionality back in

--- Server/app/controllers/courses_controller.rb
+++ Server/app/controllers/courses_controller.rb
@@ -1,24 +1,28 @@
 class CoursesController < ApplicationController
   def index
-    @course = Course.all
+    if params[:search]
+      @course = Course.search(params[:search])
+    else
+      @course = Course.all
+      
+      @courses = {}
+      @allcourses = []
+      @temp = []
+      
+      for current in @course
+        @temp.push(current.name[0,4])
+        # @allcourses << [current.name[0,4], current]
+      end
+      @uniqcourses = @temp.uniq
     
-    @courses = {}
-    @allcourses = []
-    @temp = []
-    
-    for current in @course
-      @temp.push(current.name[0,4])
-      # @allcourses << [current.name[0,4], current]
+      
+      Rails.logger.info(DateTime.now)
+      # @courses = @allcourses
+      for current in @uniqcourses
+        @courses[current] = @course.select{|x| x.name[0,4] == current }
+      end
+      Rails.logger.info(DateTime.now)
     end
-    @uniqcourses = @temp.uniq
-  
-    
-    Rails.logger.info(DateTime.now)
-    # @courses = @allcourses
-    for current in @uniqcourses
-      @courses[current] = @course.select{|x| x.name[0,4] == current }
-    end
-    Rails.logger.info(DateTime.now)
   end
   
   def destroy
